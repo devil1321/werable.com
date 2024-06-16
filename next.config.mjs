4d8e0a5dce9629761239d4a54015a9121ada693c
devil1321 <s.dominik1321@gmail.com>
2024-06-16 18:27:16 -0400

    fix vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'public, max-age=3600',
+              },
+            ],
+          },
+        ];
+      },
     images: {
         domains: ['files.cdn.printful.com'],
     }
