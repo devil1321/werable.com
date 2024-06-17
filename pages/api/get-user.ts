690b3d6aac479d6bc9355fdc33243e5bd76cc737
devil1321 <s.dominik1321@gmail.com>
2024-06-16 20:45:25 -0400

    fix

--- pages/api/get-user.ts
+++ pages/api/get-user.ts
@@ -8,13 +8,13 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse){
     if(req.method === 'POST'){
        let user = null
        let token = req.headers.authorization as string
-       if(/Bearer/gi.test(token) && token !== 'undefined'){
+       if(/Bearer/gi.test(token) && token !== 'undefined' && token !== 'null'){
            token = token?.slice(7,token.length)
        }
        if(token){
            user = jwt.verify(token as string,process.env.JWT_SECRET as string) as Interfaces.User
        }
-       if(user){
+       if(user?.id){
            try{
             const filePath = path.join(process.cwd(), 'profiles',`${user.id}.png`); 
             const image = fs.readFileSync(filePath)
