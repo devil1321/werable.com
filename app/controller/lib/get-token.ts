f2b4c8565c8a4d1ee69c3c8f4f0ba9399a54cf28
devil1321 <s.dominik1321@gmail.com>
2024-06-16 20:47:49 -0400

    fix

--- app/controller/lib/get-token.ts
+++ app/controller/lib/get-token.ts
@@ -3,7 +3,7 @@ export const getToken = () =>{
     if(typeof window !== 'undefined'){
         let allCookies = document.cookie;
         let cookiesArray = allCookies.split(';');
-        let wearableJwt = null;
+        let wearableJwt = undefined;
         for (let i = 0; i < cookiesArray.length; i++) {
             let cookie = cookiesArray[i].trim();
             if (cookie.startsWith('wearable-jwt=')) {
