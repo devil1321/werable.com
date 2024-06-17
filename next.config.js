bf4093497ccee9a006e9211d831092d92ab02367
devil1321 <s.dominik1321@gmail.com>
2024-06-16 21:05:38 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 // next.config.js
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=3600, must-revalidate',
-          },
-        ],
-      },
-    ];
-  },
   images: {
     domains: ['files.cdn.printful.com'],
   },
