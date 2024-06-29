4474b1953a0cd85d11651d46a432b64c04dd5834
devil1321 <70096468+devil1321@users.noreply.github.com>
2024-06-29 15:17:20 +0200

    Update index.ts

--- app/components/home/index.ts
+++ app/components/home/index.ts
@@ -1,11 +1,9 @@
 import Item from "../home/carousel/item.component"
 import Carousel from '../home/carousel/carousel.component'
 import Categories from "./categories.component"
-import ProductCarousel from './product-carousel'
 
 export const HomeComponents = {
     Item,
     Carousel,
-    Categories,
-    ProductCarousel
+    Categories
 }
